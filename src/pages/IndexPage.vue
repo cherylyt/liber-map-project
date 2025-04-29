0839786b3ac7a4bdd1ae1b4e97ff3b73e775389d
Cheryl Chu <98683129+cherylyt@users.noreply.github.com>
2025-04-30 01:47:15 +0800

    display openlayers map

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
+  <!-- <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
-  </q-page>
+  </q-page> -->
+  <div id="map" style="width: 100%; height: 94vh;"></div>
 </template>
 
 <script setup>
-//
+import { onMounted } from 'vue';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import 'ol/ol.css'; // Import OpenLayers CSS
+
+onMounted(() => {
+  const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+  });
+  console.log(map);
+});
+
 </script>
